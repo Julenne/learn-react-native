e84f6ec2ef5f40466db9e5e3c87e28c6a41e1d40
Arlene Pelenda <arlenej011@gmail.com>
2020-09-12 21:55:47 -0300

    middlewares

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const app = express()
 
+const saudacao = require('./saudacaoMid')
+/*
+Dois launchers utilizados:  nodemon e pm2(que é mais para produção e mais profissional)
+O pm2 é bom para microserviços
+
+a ordem das funções influenciam na resposta 
+
+O .use aceita qualquer tipo de requisição(delete, get post...)
+
+O next() é utilizado para passar para a outra função
+*/
+
+app.use(saudacao('arlene'))
+//app.listen(3000, saudacao);
+app.use((req, res, next) => {
+  console.log('Antes...')
+  next() //o next poderia ser substituido por qualquer outro nome mas ele é o mais usual no node.js
+})
+
+app.get('/opa', (req, res, next) => {
+  console.log('Durante...')
+  res.json({
+    name: 'rooi',
+    sobrenome:'leticia ne'
+  })
+  //res.send('Oi <b>estou bem</b>!');
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('Depois...')
+  next() //o next poderia ser substituido por qualquer outro nome mas ele é o mais usual no node.js
+})
+
 app.listen(3000, () => {
-  console.log('backend funcionando ')
+  console.log('backend funcionando')
 });
---
+++ exercicios-express/saudacaoMid.js
@@ -0,0 +1,8 @@
+const saudacao = (nome) => {
+  return function(req, res, next){
+    console.log(`Seja Bem-Vindo ${nome}`)
+    next()
+  }
+}
+
+module.exports = saudacao
