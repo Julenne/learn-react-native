e4029cae6e92e4516721cc975f95ebdf102c27e2
Arlene Pelenda <arlenej011@gmail.com>
2020-10-14 19:55:07 -0300

    tasklist

---
+++ tasks/src/screens/TaskList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+export default class TaksList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TaskList</Text>
+      </View>
+    )
+  }
+}
