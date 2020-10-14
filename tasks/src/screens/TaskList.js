29d2f83130fd41f8f7efdf1b96491bdf9f2aa07b
Arlene Pelenda <arlenej011@gmail.com>
2020-10-14 20:08:44 -0300

    imageBackGround

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, ImageBackground, StyleSheet } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
+
+
 export default class TaksList extends Component {
   render() {
     return (
-      <View>
-        <Text>TaskList</Text>
+      <View style={styles.container}>
+        <ImageBackground style={styles.background} source={todayImage}>
+
+        </ImageBackground>
+        <View style={styles.taskList}>
+          <Text>TaskList</Text>
+        </View>
+        
       </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  //o flex significa "crescer" na tela
+  container:{
+    flex: 1//flex ou fleGrow
+  },
+  background:{
+    flex: 3
+  },
+  taskList: {
+    flex: 7 
+  }
+})
