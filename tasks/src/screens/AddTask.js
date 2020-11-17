bbfdf05843c13a0a760239f6e883adf5946b1cda
Arlene Pelenda <arlenej011@gmail.com>
2020-11-17 17:49:36 -0300

    modal add task textinput

--- tasks/src/screens/AddTask.js
+++ tasks/src/screens/AddTask.js
@@ -3,11 +3,21 @@ import {
   Modal, 
   View, 
   Text,
+  TouchableOpacity,
+  TextInput,
   StyleSheet, 
   TouchableWithoutFeedback 
 } from 'react-native';
 import commonStyles from '../commonStyles';
+
+const initialState = { desc: ''}
+
 export default class AddTask extends Component {
+  
+  state = {
+    ...initialState
+  }
+
   render() {
     return (
       <Modal transparent={true} visible={this.props.isVisible}
@@ -19,6 +29,18 @@ export default class AddTask extends Component {
         </TouchableWithoutFeedback>
         <View style={styles.container}>
           <Text style={styles.header}>Nova tarefa</Text>
+          <TextInput style={styles.input} 
+            placeholder="Informe a Descrição..."
+            onChangeText= {desc => this.setState({ desc })}
+            value={this.state.desc}/>
+          <View style={styles.buttons}>
+            <TouchableOpacity onPress={this.props.onCancel}>
+              <Text style={styles.button}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity >
+              <Text style={styles.button}>Salvar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <TouchableWithoutFeedback
         onPress={this.props.onCancel}>
@@ -35,7 +57,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.7)',
   },
   container: {
-    flex: 1,//posso mudar este flex para o aumento do formulário da tarefa
     backgroundColor: '#FFF',
   },
   header : {
@@ -45,5 +66,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 15,
     fontSize: 18
+  },
+  input: {
+    fontFamily: commonStyles.fontFamily,
+    height: 40,
+    margin:15,
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#E3E3E3',
+    borderRadius: 6,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    margin: 20,
+    marginRight: 30,
+    color: commonStyles.colors.today,
   }
 })
