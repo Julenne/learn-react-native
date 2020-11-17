e5c4ec5a85769bfe194bbaf732c2343d64927723
Arlene Pelenda <arlenej011@gmail.com>
2020-11-17 17:30:57 -0300

    modal add task 2

--- tasks/src/screens/AddTask.js
+++ tasks/src/screens/AddTask.js
@@ -2,16 +2,24 @@ import React, {Component} from 'react';
 import {
   Modal, 
   View, 
+  Text,
   StyleSheet, 
   TouchableWithoutFeedback 
 } from 'react-native';
-
+import commonStyles from '../commonStyles';
 export default class AddTask extends Component {
   render() {
     return (
       <Modal transparent={true} visible={this.props.isVisible}
         onRequestClose={this.props.onCancel}
         animationType='slide'>
+        <TouchableWithoutFeedback
+        onPress={this.props.onCancel}>
+          <View style={styles.background}></View>
+        </TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <Text style={styles.header}>Nova tarefa</Text>
+        </View>
         <TouchableWithoutFeedback
         onPress={this.props.onCancel}>
           <View style={styles.background}></View>
@@ -25,5 +33,17 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  container: {
+    flex: 1,//posso mudar este flex para o aumento do formulário da tarefa
+    backgroundColor: '#FFF',
+  },
+  header : {
+    fontFamily: commonStyles.fontFamily,
+    backgroundColor: commonStyles.colors.today, 
+    color: commonStyles.colors.secondary,
+    textAlign: 'center',
+    padding: 15,
+    fontSize: 18
   }
 })
